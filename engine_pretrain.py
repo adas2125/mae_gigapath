f5623fb2651789b8c2d1ecef0e898f64df0a6b20
f005dg9 <f005dg9@teton.dartmouth.edu>
2024-09-17 11:54:19 -0400

    made compatible with gigapath model

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -36,16 +36,18 @@ def train_one_epoch(model: torch.nn.Module,
     if log_writer is not None:
         print('log_dir: {}'.format(log_writer.log_dir))
 
-    for data_iter_step, (samples, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+    for data_iter_step, batch in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
 
         # we use a per iteration (instead of per epoch) lr scheduler
         if data_iter_step % accum_iter == 0:
             lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
 
-        samples = samples.to(device, non_blocking=True)
+        images, img_coords = batch['imgs'], batch['coords']
+        images = images.to(device, non_blocking=True)
+        img_coords = img_coords.to(device, non_blocking=True)
 
         with torch.cuda.amp.autocast():
-            loss, _, _ = model(samples, mask_ratio=args.mask_ratio)
+            loss, _, _ = model(images, img_coords, mask_ratio=args.mask_ratio)
 
         loss_value = loss.item()
 
